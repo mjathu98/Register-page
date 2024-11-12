b087a2f2b35944c1e45706d3b08dc5cb6b475c4e
mjathu24 <jathuwork@gmail.com>
2024-11-12 20:00:58 +0530

    Login Registerpage create

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,22 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
 
-  type:string = "password";
-  isText:boolean=false;
+  type: string = "password";
+  isText: boolean = false;
   eyeIcon: string = "fa-eye-slash"
 
-  constructor(){}
+  constructor() { }
 
   ngOnInit(): void {
-    
+
   }
 
-  hideShowPass(){
-       this.isText =!this.isText;
-       this.isText?this.eyeIcon = "fa-eye" : this.eyeIcon="fa-eye-slash";
-       this.isText?this.type = "text":this.type = "password"
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password"
   }
 }
- 
